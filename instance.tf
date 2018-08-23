efaa2a69557963f7939e94f073da13d404c7714f
midebell <35881982+midebell@users.noreply.github.com>
2018-08-22 23:16:26 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -21,39 +21,4 @@ resource "aws_instance" "ctproject" {
         volume_type = "gp2"
         delete_on_termination = "true"
     }
-/*
-  provisioner "file" {
-    source = "script.sh"
-    destination = "/tmp/script.sh"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/script.sh",
-      "sudo /tmp/script.sh"
-    ]
-  }
-
-connection {
-    user = "${var.INSTANCE_USERNAME}"
-    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
-
-  }
 }
-*/
-/*
-resource "aws_ebs_volume" "ebs-volume-1" {
-    availability_zone = "us-east-1f"
-    size = 20
-    type = "gp2"
-    tags {
-        Name = "extra volume data"
-    }
-}
-
-
-resource "aws_volume_attachment" "ebs-volume-1-attachment" {
-  device_name = "/dev/xvdh"
-  volume_id = "${aws_ebs_volume.ebs-volume-1.id}"
-  instance_id = "${aws_instance.ctproject.id}"
-}
-*/
